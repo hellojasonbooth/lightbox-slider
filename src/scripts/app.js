7179f8bd9b11e54e0bda0041f14547ffc62a85e9
Jason Booth <hello@studiojasonbooth.com>
2020-11-19 13:02:24 +0000

    adding lightbox slider controls

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,35 +1,21 @@
 // lightbox shit
 const lightbox = document.querySelector('.lightbox')
 const lightboxImg = lightbox.querySelector('img')
-// const container = lightbox.querySelector('.img-container')
-
-let currentImageNum = 0
-
 const images = document.querySelectorAll('.grid img')
 
-const imageIndex = []
-imageIndex.push(images)
-
-console.log(imageIndex)
-
-function handleClick(event) {
-	let src = event.currentTarget.src
-	lightboxImg.src = src
-	lightbox.style.display = 'flex'
+for (var i = 0; i < images.length; i++) {
+	images[i].setAttribute('data-index', `${i}`)
 }
 
-
 images.forEach(img => img.addEventListener('click', handleClick))
-
 
 let imgNumber = 0
 
 const nextTag = document.querySelector(".next")
 const previousTag = document.querySelector(".prev")
- 
- 
+
 const next = function() {
-	imgNumber = imgNumber + 1
+	imgNumber++
  
 	if(imgNumber > images.length -1 ) {
 		imgNumber = 0
@@ -37,11 +23,32 @@ const next = function() {
 	updateSection()
 }
 
+const prev = function() {
+	imgNumber--
  
+	if(imgNumber < 0) {
+		imgNumber = images.length -1
+	}
+	updateSection()
+}
+
 const updateSection = function(){
 	lightboxImg.src = images[imgNumber].src
 }
  
 nextTag.addEventListener("click", next)
+previousTag.addEventListener("click", prev)
+
+function handleClick(event) {
+	let src = event.currentTarget.src
+
+	lightboxImg.src = src
+	lightbox.style.display = 'flex'
+
+	const index = event.currentTarget.getAttribute('data-index')
+
+	imgNumber = index
+}
+
 
  
